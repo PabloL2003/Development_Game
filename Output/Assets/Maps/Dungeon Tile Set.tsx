722a9b6b9da13fa5fbe1d3925c0da0cc90dc75d4
PabloL2003 <pablol.seda@gmail.com>
2023-10-30 09:12:33 +0100

    no se ni que es

---
+++ Output/Assets/Maps/Dungeon Tile Set.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.5" name="Dungeon Tile Set" tilewidth="32" tileheight="32" tilecount="63" columns="7">
+ <image source="../../../../../../Desktop/drive-download-20231030T075211Z-001/Dungeon Tile Set.png" width="240" height="288"/>
+</tileset>
