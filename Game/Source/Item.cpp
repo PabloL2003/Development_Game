723347e0ed37768fa3de0c9f640125577765920c
Jordopol <jordi.tur@estudiantat.upc.edu>
2023-11-03 14:30:23 +0100

    cambiada hitbox de los items

--- Game/Source/Item.cpp
+++ Game/Source/Item.cpp
@@ -29,7 +29,7 @@ bool Item::Start() {
 
 	//initilize textures
 	texture = app->tex->Load(texturePath);
-	pbody = app->physics->CreateCircle(position.x + 16, position.y + 16, 16, bodyType::DYNAMIC);
+	pbody = app->physics->CreateRectangle(position.x + 16, position.y + 16, 16, 16, bodyType::STATIC);
 	pbody->ctype = ColliderType::ITEM;
 
 	return true;
