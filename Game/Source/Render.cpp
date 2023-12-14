2e82294f2036b7fe2269747ccb908fed011c30a1
PabloL2003 <125266074+PabloL2003@users.noreply.github.com>
2023-12-14 17:45:22 +0100

    fd

--- Game/Source/Render.cpp
+++ Game/Source/Render.cpp
@@ -175,7 +175,7 @@ bool Render::DrawTexture(SDL_Texture* texture, int x, int y, const SDL_Rect* sec
 	if(SDL_RenderCopyEx(renderer, texture, section, &rect, angle, p, SDL_FLIP_NONE) != 0)
 	{
 		//Muteado de momento hasta que se arregle lo del mapa
-		/*LOG("Cannot blit to screen. SDL_RenderCopy error: %s", SDL_GetError());*/ 
+		LOG("Cannot blit to screen. SDL_RenderCopy error: %s", SDL_GetError());
 		ret = false;
 	}
 
